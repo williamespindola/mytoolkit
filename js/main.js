e47d561e4bb737e53a4a22ce68dcebfa41d33bb9
William Espindola <williamespindola@instinfo.com.br>
2013-09-28 17:17:27 -0300

    fist stabel version

---
+++ js/main.js
@@ -0,0 +1,41 @@
+require.config({
+    baseUrl: 'js/lib',
+    paths : {hgn: 'hgn'},
+    hgn: {templateExtension: ".html"}
+});
+
+require(
+    [
+        'mout/src/object/size', 
+        'mout/src/array/forEach', 
+        'json!../../tools.json',
+        'hgn!../../quantity-template',
+        'hgn!../../project-template'
+    ],
+    function(size, forEach, tools, template, projectTemplate){
+        var data = {"quantity": {
+                        "name": "total projects", 
+                        "number":size(tools)
+                    }};
+
+        var projects = document.getElementById('projects')
+                               .innerHTML = template(data);
+
+        document.getElementById('search')
+                .addEventListener("keyup", function(){
+                    var value = this.value;
+                    document.getElementById('projects').innerHTML  = '';
+                    document.getElementById('look')
+                            .innerHTML  = 'look for ' + value;
+                    tools.filter(function(a){
+                        forEach(a.meta,function(meta){
+                            if (meta == value){
+                                document.getElementById('projects')
+                                        .innerHTML += projectTemplate(a);
+                            }
+                        });
+                    });
+                }, false);
+    }
+);
+
